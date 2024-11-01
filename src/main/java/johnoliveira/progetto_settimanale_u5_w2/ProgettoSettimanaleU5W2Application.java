ca0035c4a6e5f6ef406422c01c80a04dc8d37b63
John-9813 <john.oliver98.br@gmail.com>
2024-11-01 16:16:42 +0100

    Initial commit

---
+++ src/main/java/johnoliveira/progetto_settimanale_u5_w2/ProgettoSettimanaleU5W2Application.java
@@ -0,0 +1,13 @@
+package johnoliveira.progetto_settimanale_u5_w2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimanaleU5W2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimanaleU5W2Application.class, args);
+	}
+
+}
---
+++ src/test/java/johnoliveira/progetto_settimanale_u5_w2/ProgettoSettimanaleU5W2ApplicationTests.java
@@ -0,0 +1,13 @@
+package johnoliveira.progetto_settimanale_u5_w2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimanaleU5W2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
